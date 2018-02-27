5fe4ef473d79a3613fb8eb624a8cac9114f65a8b
KellyStamps <kellydsample@gmail.com>
2018-02-27 14:28:22 -0500

    sorted cards by highest rated, implemented search feature for challenges

--- src/components/ChallengeList.js
+++ src/components/ChallengeList.js
@@ -4,16 +4,35 @@ import {Link} from 'react-router-dom'
 import { connect } from 'react-redux';
 
 class ChallengeList extends React.Component {
+  
+  state = {
+    searchTerm: ''
+  }
+  
+  handleChange = (event) => {
+    this.setState({searchTerm: event.target.value.toLowerCase()})
+  }
+  
+  renderHelper = () => {
+    let list;
+    if (this.state.searchTerm.length > 0) {
+      list = this.props.challenges.filter(chal => chal.content.toLowerCase().includes(this.state.searchTerm))
+    } else {
+      list = this.props.challenges.sort((a,b) => {
+        return b.rating - a.rating
+      });
+    }
+    return list;
+  }
 
   render() {
-    const list = this.props.challenges.sort((a,b) => {
-      return a.rating - b.rating
-    })
       return this.props.user ? (
       <div className="challenge-list">
-        {list.map(chal => (
-          <ChallengeCard challenge={chal} key={chal.id}/>
-        ))}
+        <div className='search'>
+          <input onChange={this.handleChange} value={this.state.searchTerm} type='text' placeholder='Search Challenges...' />
+        </div>
+        {this.renderHelper().map(chal => (<ChallengeCard challenge={chal} key={chal.id}/>))}
+        
       </div>
     ) : (
       <div className="log-in-reminder">

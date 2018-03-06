ea18891a069de5c136b5d2250e5f1182ba7a77fe
KellyStamps <kellydsample@gmail.com>
2018-03-06 13:55:21 -0500

    added styling, changed look of completed challenge

--- src/components/MyCompletedChallengeShow.js
+++ src/components/MyCompletedChallengeShow.js
@@ -6,37 +6,29 @@ import {Link} from 'react-router-dom'
 class MyInProgressChallengeShow extends React.Component {
   render(){
     
-    let wholeChallenge;
-    this.props.user ? (
-      wholeChallenge = this.props.user.favorites.find(fav => fav.challenge.id === parseInt(this.props.match.params.id,10))
-    ) : null
-
-
-    return this.props.user ? (
-      <div className='my-challenge-show-div'>
+    if (this.props.user) {
+      let wholeChallenge = this.props.user.favorites.find(fav => fav.challenge.id === parseInt(this.props.match.params.id,10))
       
-        <div className='my-challenge-headline'>
-          <h1>{wholeChallenge.challenge.content}</h1>
-          <p id='show-rating'>Rating: {wholeChallenge.challenge.rating}/10</p>
-        </div>
-        
-        <div className='in-progress-links-div'>
-          <p>Links:</p> 
-          {wholeChallenge.git_link && wholeChallenge.live_link  !== null ? (
+      return (
+        <div className='my-comp-challenge-show-div'>
+          <div className='my-challenge-headline'>
+            <h1>{wholeChallenge.challenge.content}</h1>
+            <p id='show-rating'>Rating: {wholeChallenge.challenge.rating}/10</p>
+          </div>
+            
+          <div className='comp-links-div'>
             <div>
-              <p>
-                <a href={`http://${wholeChallenge.git_link}`} target='_blank'>{wholeChallenge.git_link }</a>
-              </p>
-              <p>
-                <a href={wholeChallenge.live_link} target='_blank'>{wholeChallenge.live_link }</a>
-              </p>
-              </div>) : <p>no links yet</p>}
+              <p>See it on Github: <a href={`http://${wholeChallenge.git_link}`} target='_blank'>{wholeChallenge.git_link }</a></p>
+              <p>See it in Action: <a href={wholeChallenge.live_link} target='_blank'>{wholeChallenge.live_link }</a></p>
+            </div> 
+          </div>  
         </div>
-        
-      </div>
-    ) : (
-      <div className="log-in-reminder"><h1>Please <Link to='/'>log in</Link>  to view challenges</h1></div>
-    )
+      )
+    } else {
+      return (
+        <div className="log-in-reminder"><h1>Please <Link to='/'>log in</Link>  to view challenges</h1></div>
+      )
+    }
   }
 }
 
--- src/components/UsersIndexContainer.js
+++ src/components/UsersIndexContainer.js
@@ -3,14 +3,34 @@ import {connect} from 'react-redux'
 import UserIndexCard from './UserIndexCard'
 
 class UsersIndexContainer extends React.Component {
+  
+  state = {
+    searchTerm: ''
+  }
+  
+  handleChange = (event) => {
+    this.setState({searchTerm: event.target.value.toLowerCase()})
+  }
+  
+  renderHelper = () => {
+    if (this.state.searchTerm.length > 0) {
+      return this.props.allUsers.filter(friend => friend.user.username.toLowerCase().includes(this.state.searchTerm))
+    } else {
+      return this.props.allUsers.filter(user => user.user.id !== this.props.user.id)
+    }
+  }
+  
   render(){
-    const otherUsers = this.props.allUsers.filter(user => user.user.id !== this.props.user.id)
     
     return(
       <div className='users-index'>
-        <h1>Add Friends!</h1>
+        <h1>Add Friends!</h1>    
+        <div className='user-search'>
+          <input onChange={this.handleChange} value={this.state.searchTerm} id='search' type='text' placeholder='Search Friends...' />
+        </div>
+        
         <div className='users-index-container'>
-          {otherUsers.map(user=> <UserIndexCard key={user.user.id} user={user}/>)}
+          {this.renderHelper().map(user=> <UserIndexCard key={user.user.id} user={user}/>)}
         </div>
       </div>
     )
@@ -20,5 +40,6 @@ class UsersIndexContainer extends React.Component {
 const mapStateToProps = (state) => {
   return {...state.users}
 }
+
 
 export default connect(mapStateToProps)(UsersIndexContainer)

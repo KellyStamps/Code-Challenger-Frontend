d57f8d813282b38b79b86a8902bb16accb5c1821
KellyStamps <kellydsample@gmail.com>
2018-03-07 17:03:31 -0500

    playing around with responsive design

--- src/components/ChallengeList.js
+++ src/components/ChallengeList.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom'
 import { connect } from 'react-redux';
 
 class ChallengeList extends React.Component {
-  
+
   state = {
     searchTerm: '',
     checked: false
   }
-  
+
   handleChange = (event) => {
     if (event.target.id === 'search') {
       this.setState({searchTerm: event.target.value.toLowerCase()})
@@ -17,7 +17,7 @@ class ChallengeList extends React.Component {
         this.setState({checked: event.target.checked})
     }
   }
-  
+
   renderHelper = () => {
     let list;
     if (this.state.searchTerm.length > 0) {
@@ -25,15 +25,15 @@ class ChallengeList extends React.Component {
     } else {
       if (this.state.checked){
         let faveIds = this.props.user.favorites.map(fav => fav.challenge.id)
-        
+
         let allChallengeIds = this.props.challenges.map(chal => chal.id)
-        
+
         let ids = allChallengeIds.filter(function(n) {
           return faveIds.indexOf(n) === -1;
         })
-        
+
         list = this.props.challenges.filter(chal => {
-            return ids.indexOf(chal.id) > -1	
+            return ids.indexOf(chal.id) > -1
         }).sort((a,b) => {
           return b.rating - a.rating
         });
@@ -48,25 +48,24 @@ class ChallengeList extends React.Component {
 
   render() {
     if (this.props.user) {
-  
+
       return (
       <div className="challenge-list">
-      
-      <div className='filter-cards'>
-        <label htmlFor='filter-checkbox'>Only projects I have not done</label>
-        <input onChange={this.handleChange} id='filter-checkbox' type='checkbox'/>
-      </div>
-    
-      <div className='search'>
-        <input onChange={this.handleChange} value={this.state.searchTerm} id='search' type='text' placeholder='Search Challenges...' />
-      </div>
-      
-      <div className='new-challenge-link-div'>
-        <Link to='/challenges/new'>New Challenge</Link>
-      </div>
-        
-      {this.renderHelper().map(chal => (<ChallengeCard challenge={chal} key={chal.id}/>))}
-        
+        <div className='filter-cards'>
+          <label htmlFor='filter-checkbox'>Only projects I have not done</label>
+          <input onChange={this.handleChange} id='filter-checkbox' type='checkbox'/>
+        </div>
+
+        <div className='search'>
+          <input onChange={this.handleChange} value={this.state.searchTerm} id='search' type='text' placeholder='Search Challenges...' />
+        </div>
+
+        <div className='new-challenge-link-div'>
+          <Link to='/challenges/new'>New Challenge</Link>
+        </div>
+
+          {this.renderHelper().map(chal => (<ChallengeCard challenge={chal} key={chal.id}/>))}
+
       </div> )
     } else {
       return (
@@ -74,7 +73,7 @@ class ChallengeList extends React.Component {
           <h1>Please <Link to='/'>log in</Link>  to view challenges</h1>
         </div>
       )
-    }  
+    }
   }
 }
 
